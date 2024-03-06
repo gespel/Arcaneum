150ff8676511a68f42da944e0fc8c558cc9f171a
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2024-03-06 14:29:00 +0100

    arrow shooting

--- src/arcaneum/base/Events.kt
+++ src/arcaneum/base/Events.kt
@@ -1,10 +1,15 @@
 package arcaneum.base
 
 import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.entity.Arrow
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerLevelChangeEvent
+import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemStack
 
 class Events : Listener {
     @EventHandler
@@ -20,5 +25,13 @@ class Events : Listener {
         if(old < new) {
             event.player.sendMessage("${ChatColor.GREEN}Level up! Level: ${ChatColor.GOLD}${event.player.level}")
         }
+    }
+
+    @EventHandler
+    fun onSpellCast(event: PlayerInteractEvent) {
+        if(event.player.inventory.itemInMainHand == ItemStack(Material.STONE_SWORD, 1) && event.hand?.equals(EquipmentSlot.HAND) == true) {
+            event.player.launchProjectile(Arrow::class.java, event.player.location.direction)
+        }
+
     }
 }
